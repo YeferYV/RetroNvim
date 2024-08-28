7ebc8567733788abeb90e810eb48643296167b3e
Yefer YV <yucrayeff36@gmail.com>
2024-08-28 12:43:43 -0500

    feat(extension.js): adding "vscode-neovim.neovimExecutablePath" for Windows and MacOS

--- extension.js
+++ extension.js
@@ -5,13 +5,29 @@ const os = require('os');
 function setNeovimPath() {
   // Construct the dynamic path
   const homeDirectory = os.homedir();
-  const nvimPath = path.join(homeDirectory, '.vscode/extensions/yeferyv.retronvim-0.0.4/bin/nvim');
+  const nvimPathLinux = path.join(homeDirectory, '.vscode/extensions/yeferyv.retronvim-0.0.4/bin/nvim');
+  const nvimPathMacOS = path.join(homeDirectory, '.vscode/extensions/yeferyv.retronvim-0.0.4/bin/nvim-macos-x86_64/bin/nvim');
+  const nvimPathWindows = path.join(homeDirectory, '.vscode/extensions/yeferyv.retronvim-0.0.4/bin/nvim-win64/bin/nvim.exe');
 
   // Access the configuration for 'vscode-neovim'
   const config = vscode.workspace.getConfiguration('vscode-neovim');
 
   // Update the 'neovimExecutablePaths.linux' setting
-  config.update('neovimExecutablePaths.linux', nvimPath, vscode.ConfigurationTarget.Global)
+  config.update('neovimExecutablePaths.linux', nvimPathLinux, vscode.ConfigurationTarget.Global)
+    .then(() => {
+      vscode.window.showInformationMessage(`Neovim path set to: ${nvimPath}`);
+    }, (err) => {
+      vscode.window.showErrorMessage(`Failed to set Neovim path: ${err}`);
+    });
+
+  config.update('neovimExecutablePaths.darwin', nvimPathMacOS, vscode.ConfigurationTarget.Global)
+    .then(() => {
+      vscode.window.showInformationMessage(`Neovim path set to: ${nvimPath}`);
+    }, (err) => {
+      vscode.window.showErrorMessage(`Failed to set Neovim path: ${err}`);
+    });
+
+  config.update('neovimExecutablePaths.win32', nvimPathWindows, vscode.ConfigurationTarget.Global)
     .then(() => {
       vscode.window.showInformationMessage(`Neovim path set to: ${nvimPath}`);
     }, (err) => {
