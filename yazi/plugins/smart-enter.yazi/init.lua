225d57ffa43183746ef09b9b541c1f8bc35ca61b
Yefer YV <yucrayeff36@gmail.com>
2024-12-13 22:16:57 -0500

    chore(yazi): migrating to yazi v0.4.2

--- yazi/plugins/smart-enter.yazi/init.lua
+++ yazi/plugins/smart-enter.yazi/init.lua
@@ -1,6 +1,7 @@
+--- @sync entry
 return {
-	entry = function()
-		local h = cx.active.current.hovered
-		ya.manager_emit(h and h.cha.is_dir and "enter" or "open", { hovered = true })
-	end,
+  entry = function()
+    local h = cx.active.current.hovered
+    ya.manager_emit(h and h.cha.is_dir and "enter" or "open", { hovered = true })
+  end,
 }
