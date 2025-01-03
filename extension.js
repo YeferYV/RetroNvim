5ab750799dde530a12e333e5518041187dd5595d
Yefer YV <yucrayeff36@gmail.com>
2025-01-03 18:24:13 -0500

    ref(extension.js): creating symlink from context.extensionPath

--- extension.js
+++ extension.js
@@ -3,55 +3,45 @@ const path = require('path');
 const os = require('os');
 const fs = require('fs');
 
-function setNeovimPath() {
+function setNeovimPath(homeExtension) {
   // Construct the dynamic path
   const homeDirectory = os.homedir();
-  const nvimPathLinux = path.join(homeDirectory, '.vscode/extensions/yeferyv.retronvim-0.1.20241228/bin/linux-x64/nvim');
-  const nvimPathMacOS = path.join(homeDirectory, '.vscode/extensions/yeferyv.retronvim-0.1.20241228/bin/darwin-x64/nvim-macos-x86_64/bin/nvim');
-  const nvimPathWindows = path.join(homeDirectory, '.vscode/extensions/yeferyv.retronvim-0.1.20241228/bin/win32-x64/nvim-win64/bin/nvim.exe');
-  const nvimPathWindowsRoot = path.join(homeDirectory, '.vscode/extensions/yeferyv.retronvim-0.1.20241228');
-  const nvimPathWindowsRootX64 = path.join(homeDirectory, '.vscode/extensions/yeferyv.retronvim-0.1.20241228-win32-x64');
+  const retronvimPath = path.join(homeDirectory, '.vscode/extensions/yeferyv.retronvim');
+  const nvimPathLinux = path.join(homeDirectory, '.vscode/extensions/yeferyv.retronvim/bin/linux-x64/nvim');
+  const nvimPathMacOS = path.join(homeDirectory, '.vscode/extensions/yeferyv.retronvim/bin/darwin-x64/nvim-macos-x86_64/bin/nvim');
+  const nvimPathWindows = path.join(homeDirectory, '.vscode/extensions/yeferyv.retronvim/bin/win32-x64/nvim-win64/bin/nvim.exe');
 
-  // create symlink if installing from marketplace on Windows 10/11
-  if (os.platform() == "win32" && fs.existsSync(nvimPathWindowsRootX64)) {
-    fs.symlinkSync(nvimPathWindowsRootX64, nvimPathWindowsRoot)
+  // create symlink for environment variables path
+  if (fs.existsSync(retronvimPath)) {
+    fs.unlinkSync(retronvimPath)
+    fs.symlinkSync(homeExtension, retronvimPath)
+  } else {
+    fs.symlinkSync(homeExtension, retronvimPath)
   }
 
   // Access the configuration for 'vscode-neovim'
   const config = vscode.workspace.getConfiguration();
 
-  // Update the 'neovimExecutablePaths.linux' setting
-  config.update('vscode-neovim.neovimExecutablePaths.linux', nvimPathLinux, vscode.ConfigurationTarget.Global)
-    .then(() => {
-      vscode.window.showInformationMessage(`Neovim path set to: ${nvimPathLinux}`);
-    }, (err) => {
-      vscode.window.showErrorMessage(`Failed to set Neovim path: ${err}`);
-    });
-
-  config.update('vscode-neovim.neovimExecutablePaths.darwin', nvimPathMacOS, vscode.ConfigurationTarget.Global)
-    .then(() => {
-      vscode.window.showInformationMessage(`Neovim path set to: ${nvimPathMacOS}`);
-    }, (err) => {
-      vscode.window.showErrorMessage(`Failed to set Neovim path: ${err}`);
-    });
-
-  config.update('vscode-neovim.neovimExecutablePaths.win32', nvimPathWindows, vscode.ConfigurationTarget.Global)
-    .then(() => {
-      vscode.window.showInformationMessage(`Neovim path set to: ${nvimPathWindows}`);
-    }, (err) => {
-      vscode.window.showErrorMessage(`Failed to set Neovim path: ${err}`);
-    });
-
   // config.update("telemetry.telemetryLevel", "off", vscode.ConfigurationTarget.Global)
   // config.update('window.titleBarStyle', "custom", vscode.ConfigurationTarget.Global)
   config.update("security.workspace.trust.untrustedFiles", "open", vscode.ConfigurationTarget.Global)
   config.update('window.customMenuBarAltFocus', false, vscode.ConfigurationTarget.Global) // Windows's alt sometimes conflicts with whichkey
+  config.update('vscode-neovim.neovimExecutablePaths.linux', nvimPathLinux)
+  config.update('vscode-neovim.neovimExecutablePaths.darwin', nvimPathMacOS)
+  config.update('vscode-neovim.neovimExecutablePaths.win32', nvimPathWindows)
 
 }
 
 // You can call this function in your extension's activate function or based on certain events
 function activate(context) {
-  setNeovimPath();
+  // https://stackoverflow.com/questions/44113025/how-to-dynamically-query-my-vscode-extension-version-from-the-extension-code
+  // vscode.window.showInformationMessage(context.extension.packageJSON.version);
+
+  // https://stackoverflow.com/questions/39569993/vs-code-extension-get-full-path
+  setNeovimPath(context.extensionPath);
+  // vscode.window.showInformationMessage(context.extensionUri);
+  // vscode.window.showInformationMessage(context.extensionPath);
+  // vscode.window.showInformationMessage(context.extension);
 }
 
 exports.activate = activate;
